79e51c09ef2670d8d39e52ebc79172f296a63ce0
Thibauld Favre <tfavre@gmail.com>
2016-04-12 04:49:10 -0400

    * fix previous_state exception

--- bot/users.rb
+++ bot/users.rb
@@ -279,7 +279,7 @@ END
 			return nil if screen.nil?
 			screen=Hash[screen.map{|(k,v)| [k.to_sym,v]}] # pas recursif
 			screen[:kbd_options]=Hash[screen[:kbd_options].map{|(k,v)| [k.to_sym,v]}] unless screen[:kbd_options].nil?
-			@users[user_id]['session']=user['session']['previous_session'].clone
+			@users[user_id]['session']=user['session']['previous_session'].clone unless user['session']['previous_session'].nil?
 			return screen
 		end
 	end
